eea080471fc9aff47353c63e597c31df65742920
miguel <cueiras98@gmail.com>
2019-05-12 17:45:20 +0200

    añado el css de la plantilla descripcion leer!!
    falta mirar que hacer con el fondo si dejar esa foto o que salga la del piso

--- index.php
+++ index.php
@@ -3,30 +3,89 @@
 <head>
 	<title></title>
 	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-	<link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <link rel="stylesheet" href="css/magnific-popup.css">
+    <link rel="stylesheet" href="css/jquery-ui.css">
+    
+    <link rel="stylesheet" href="css/owl.theme.default.min.css">
+    <link rel="stylesheet" href="css/bootstrap-datepicker.css">
+    <link rel="stylesheet" href="css/mediaelementplayer.css">
+    <link rel="stylesheet" href="css/animate.css">
+    <link rel="stylesheet" href="fonts/flaticon/font/flaticon.css">
+    <link rel="stylesheet" href="css/fl-bigmug-line.css">
+    
+  
+    
+
+    <link rel="stylesheet" href="css/style.css">
 </head>
 <body>
-<div class="navbar" style='background-image: url(./images/hero_bg_1.jpg);'>
-	
-        <div class=" col-7 pt-4 ml-4 mb-5 col-md-8 col-lg-4">
-              <h1 class="mb-0"><a href="index.html" class="h2 mb-0"><strong>ALQUILEA<span class="text-danger">.</span></strong></a></h1>
+	<div class='navbar' style='background-image: url(./images/hero_bg_1.jpg);'>
+	<div class='col-7 pt-4 ml-4 mb-5 col-md-8 col-lg-4'>
+    <h1 class='mb-0'><a href='index.html' class='h2 mb-0'><strong>ALQUILEA<span class='text-danger'>.</span></strong></a></h1>
         </div>
-        <div class=" col-5 pt-4 mr-4 mb-5">
-            <a href="index.html">Incio</a>
-			<a href="buy.html">Casas en alquiler</a>
-            <a href="rent.html">Preguntas frecuentes</a>
+    <div class='col-5 pt-4 mr-4 mb-5' id='menu1'>
+    <a href='index.html'>Incio</a>
+			<a href='buy.html'>Casas en alquiler</a>
+            <a href='rent.html'>Preguntas frecuentes</a>
                   
-            <a href="properties.html">Conocenos</a> 
-            <a href="contact.html">Contacto</a>
+            <a href='properties.html'>Conocenos</a> 
+            <a href='contact.html'>Contacto</a>
         </div>
-		<div class="col-12">
+	<div class='col-12'>
  		<?php 
- 		include './funciones_php/generar_carrousel.php';
+ 		include './funciones_php/generar_menu.php';
+
  		?>
-		</div>
-
-</div>
-
+ 	</div>
+	<div class="site-section site-section-sm pb-0">
+      <div class="container">
+        <div class="row">
+          <form class="form-search col-md-12" style="margin-top: -100px;">
+            <div class="row  align-items-end">
+              <div class="col-md-3">
+                <label for="list-types">Listing Types</label>
+                <div class="select-wrap">
+                  <span class="icon icon-arrow_drop_down"></span>
+                  <select name="list-types" id="list-types" class="form-control d-block rounded-0">
+                    <option value="">Condo</option>
+                    <option value="">Commercial Building</option>
+                    <option value="">Land Property</option>
+                  </select>
+                </div>
+              </div>
+              <div class="col-md-3">
+                <label for="offer-types">Offer Type</label>
+                <div class="select-wrap">
+                  <span class="icon icon-arrow_drop_down"></span>
+                  <select name="offer-types" id="offer-types" class="form-control d-block rounded-0">
+                    <option value="">For Sale</option>
+                    <option value="">For Rent</option>
+                    <option value="">For Lease</option>
+                  </select>
+                </div>
+              </div>
+              <div class="col-md-3">
+                <label for="select-city">Select City</label>
+                <div class="select-wrap">
+                  <span class="icon icon-arrow_drop_down"></span>
+                  <select name="select-city" id="select-city" class="form-control d-block rounded-0">
+                    <option value="">New York</option>
+                    <option value="">Brooklyn</option>
+                    <option value="">London</option>
+                    <option value="">Japan</option>
+                    <option value="">Philippines</option>
+                  </select>
+                </div>
+              </div>
+              <div class="col-md-3">
+                <input type="submit" class="btn btn-success text-white btn-block rounded-0" value="Search">
+              </div>
+            </div>
+          </form>
+        </div>  
+      </div>
+    </div>
 
 </body>
 <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
